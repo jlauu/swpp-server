e905db9c35db7debc3e5d22c4f60469a132245c1
opaqe <johnny.df.lau@gmail.com>
2016-06-06 11:52:32 -0400

    set up posting json data

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, json, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+app.last = ""
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+@app.route('/', methods=['GET'])
+def index():
+    return "Last message received: " + app.last
+
+@app.route('/send',methods=['POST'])
+def send():
+    if request.headers['Content-Type'] == 'application/json':
+        data = json.dumps(request.json)
+        app.pv = data
 
 if __name__ == '__main__':
     app.run()
-
