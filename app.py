3a5e38b1f242d5db5bfa1d1a7bdc74024e2a1326
opaqe <johnny.df.lau@gmail.com>
2016-06-06 14:18:35 -0400

    Moved models.py into app.py

--- app.py
+++ app.py
@@ -2,14 +2,34 @@ import os
 from flask import Flask, request, json, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
-from models import *
-
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 app.last = ""
 
+class PageVisit(db.Model):
+    __tablename__ = 'pagevisits'
+    id = db.Column(db.Integer, primary_key=True)
+    sessionid = db.Column(db.Integer)
+    tabid = db.Column(db.Integer)
+    windowid = db.Column(db.Integer)
+    srcid = db.Column(db.Integer)
+    url = db.Column(db.String())
+    time = db.Column(db.Float())
+    transition = db.Column(db.String())
+
+    def __init__(json):
+        self.sessionid = json['sessionID']
+        self.tabid = json['tabid']
+        self.windowid = json['windowid']
+        self.srcid = json['srcid']
+        self.url = json['url']
+        self.time = json['time']
+        self.transition = json['transition']
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
 @app.route('/', methods=['GET'])
 def index():
     return "Last message received: " + app.last
