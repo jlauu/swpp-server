002062e583881cb43e9e367c9f21f645b6ba6606
opaqe <johnny.df.lau@gmail.com>
2016-06-22 16:58:47 -0400

    force layout graph. needs to fix groupings

--- scripts/Graph.py
+++ scripts/Graph.py
@@ -1,18 +1,26 @@
 class Graph:
     def __init__(self):
-        self.nodes = []
-        self.adjlist = {}
-        self.ids = {}
+        self.nodes = {} # id -> elem
+        self.adjlist = {} # id -> [id]
+        self.ids = {} # elem -> id
         self.uuid = 1
+        self.groups = {} # id -> group
+        self._ngroups = 0
+
+    def getNodes(self):
+        for i,n in self.nodes.items():
+            yield n
 
     def getID (self, elem):
         return self.ids[elem]
 
     def addNode(self, n):
-        if n not in self.nodes:
+        if n not in self.ids:
             self.ids[n] = self.uuid
+            self.nodes[self.uuid] = n
+            self.groups[self.uuid] = self._ngroups
             self.uuid += 1
-            self.nodes.append(n)
+            self._ngroups += 1
         return self.ids[n]
 
     def addEdge(self, src, dest):
@@ -23,6 +31,12 @@ class Graph:
             self.addNode(dest)
             self.adjlist[self.getID(dest)] = []
         edges = self.adjlist[self.getID(src)]
+        gs = [self.groups[self.getID(n)] for n in [src,dest]]
+        if gs[0] != gs[1]:
+            group = min(gs)
+            self.groups[src] = group
+            self.groups[dest] = group
+            self._ngroups -= 1
         node = self.getID(dest)
         if node not in edges:
             edges.append(self.getID(dest))
--- scripts/formatGraphJSON.py
+++ scripts/formatGraphJSON.py
@@ -22,8 +22,8 @@ def main(filename):
 def outputGraphFiles(gid, graph):
     nodes = []
     links = []
-    for url, index in graph.ids.items():
-        nodes.append({'url':url, 'id': index})
+    for n_id in graph.nodes:
+        nodes.append({'url':graph.nodes[n_id], 'id': n_id, 'group':graph.groups[n_id]})
     for src, dests in graph.adjlist.items():
         for dest in dests:
             if dest:
