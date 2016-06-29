b397e6a5c7614247fa9987fd6f849cdc58035cb7
opaqe <johnny.df.lau@gmail.com>
2016-06-29 11:29:36 -0400

    Temporary cluster files

---
+++ tmp/update_groups.py
@@ -0,0 +1,20 @@
+from app import db
+from sqlalchemy.sql import text
+
+with open('url-cluster-em-100words.csv') as f:
+    skip = True
+    urls = []
+    for line in f:
+        if skip:
+            skip = False
+            continue
+        parts = line.rpartition(',')
+        group = parts[-1].strip()
+        url = parts[0].partition(',')[-1].strip()
+        urls.append((url,int(group)))
+    
+def mkq(u,g):
+    return text("INSERT INTO keywords (url, keywords, cluster) VALUES (:u, '{}', :g) ON CONFLICT (url) DO UPDATE SET cluster = EXCLUDED.cluster;").bindparams(u=u,g=g)
+
+for u, g in urls:
+    db.engine.execute(mkq(u,g))
