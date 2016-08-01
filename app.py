2adf44654f6c1a4f30108130cddd4e5c47cd1e21
opaqe <johnny.df.lau@gmail.com>
2016-08-01 10:04:24 -0400

    Added GET single cluster

--- app.py
+++ app.py
@@ -17,12 +17,13 @@ def index():
   if request.args.get('uid') is not None:
       userid = request.args.get('uid')
       graphs = UserCluster.query.filter_by(userid=userid).all()
+      name = request.args.get('name')
       return json.dumps([{
           'id' : g.id,
           'name': g.name, 
           'keywords': g.keywords, 
           'graph': g.cluster
-      } for g in graphs])
+      } for g in graphs if not name or name == g.name])
   else:
       return ""
 
